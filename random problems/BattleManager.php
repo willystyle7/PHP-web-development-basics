77812df03d79fa45576d5f5ac58792364eaf29a7
Ilia Vasilev Vasilev <ilia111@yahoo.com>
2019-09-28 16:16:40 +0300

    some more

--- random problems/BattleManager.php
+++ random problems/BattleManager.php
@@ -2,36 +2,32 @@
 $input = readline();
 $battles = [];
 while ($input != "Results") {
-    $comand = explode(":", $input);
-    switch ($comand[0]) {
+    $command = explode(":", $input);
+    switch ($command[0]) {
         case "Add":
-            $personName = $comand[1];
-            $health = $comand[2];
-            $energy = $comand[3];
+            $personName = $command[1];
+            $health = intval($command[2]);
+            $energy = intval($command[3]);
             if (!key_exists($personName, $battles)) {
-                // $battles[$personName] = [];
+                $battles[$personName] = [];
                 $battles[$personName]['health'] = 0;
                 $battles[$personName]['energy'] = $energy;
             }
             $battles[$personName]['health'] += $health;
-            // $battles[$personName]['energy'] = $energy;
             break;
         case "Delete":
-            $personName = $comand[1];
+            $personName = $command[1];
             if ($personName == "All") {
-                foreach ($battles as $key => $val) {
-                    unset($battles[$key]);
-                }
+                $battles = [];
             }
             if (key_exists($personName, $battles)) {
                 unset($battles[$personName]);
             }
             break;
         case "Attack":
-            // Attack:Clark:Mark:500
-            $attackerName = $comand[1];
-            $defenderName = $comand[2];
-            $damage = $comand[3];
+            $attackerName = $command[1];
+            $defenderName = $command[2];
+            $damage = intval($command[3]);
             if (key_exists($attackerName, $battles) && key_exists($defenderName, $battles)) {
                 $battles[$defenderName]['health'] -= $damage;
                 $battles[$attackerName]['energy'] -= 1;
@@ -50,23 +46,20 @@ while ($input != "Results") {
 }
 $count = count($battles);
 echo "People count: $count\n";
-// ksort($battles);
-uksort($battles, 'strcasecmp');
-// ksort($battles, SORT_STRING | SORT_FLAG_CASE);
-// uksort($battles, function ($a, $b) { return $a <=> $b; });
 
-// arsort($battles);
-// uasort($battles, 'cmp');
-uasort($battles, function ($a, $b) { return $b['health'] - $a['health']; });
+uksort($battles, 'cmp');
+
+function cmp($a, $b)
+{
+    if ($GLOBALS['battles'][$a]['health'] == $GLOBALS['battles'][$b]['health']) {
+        if ($a == $b) {
+            return 0;
+        }
+        return strcmp($a, $b);
+    }
+    return $GLOBALS['battles'][$a]['health'] - $GLOBALS['battles'][$b]['health'] > 0 ? -1 : 1;
+}
 
 foreach ($battles as $key => $val) {
     echo "$key - ${val['health']} - ${val['energy']}\n";
 }
-
-function cmp($a, $b) {
-    if ($a['health'] == $b['health']) {
-        return 0;
-    }
-    return $a['health'] - $b['health'] > 0 ? -1 : 1;
-}
-
--- random problems/PokemonDontGo.php
+++ random problems/PokemonDontGo.php
@@ -26,3 +26,46 @@ while (count($arr) > 0) {
     }
 }
 echo $sum;
+
+// <?php
+// $arr = array_map('intval', explode(' ', readline()));
+// $arrDeletedElements = [];
+// while (true) {
+//     if (count($arr) == 0) {
+//         $sum = 0;
+//         for($i = 0; $i < count($arrDeletedElements); $i++) {
+//             $sum += $arrDeletedElements[$i];
+//         }
+//         echo $sum;
+//         return;
+//     }
+//     $index = intval(readline());
+// 	if ($index >= 0 && $index < count($arr)) {
+// 		$valueElement = $arr[$index];
+//         array_splice($arr, $index, 1);
+//         $arrDeletedElements[] = $valueElement;
+//         $arr = increaseOrDecrease($arr, $valueElement);
+// 	} else if ($index < 0) {
+// 		$valueElement = $arr[0];
+//         array_splice($arr, 0, 1, $arr[count($arr)-1]);
+//         $arrDeletedElements[] = $valueElement;
+//         $arr = increaseOrDecrease($arr, $valueElement);
+// 	} else if ($index > count($arr)-1) {
+// 		$valueElement = $arr[count($arr)-1];
+//         array_splice($arr, count($arr)-1, 1, $arr[0]);
+//         $arrDeletedElements[] = $valueElement;
+//         $arr = increaseOrDecrease($arr, $valueElement);
+// 	}
+// }
+
+// function increaseOrDecrease($array, $deletedEle)
+// {
+//     for($i = 0; $i < count($array); $i++) {
+//         if ($array[$i] <= $deletedEle) {
+//             $array[$i] += $deletedEle;
+//         } else {
+//             $array[$i] -= $deletedEle;
+//         }
+//     }
+//     return $array;
+// }
